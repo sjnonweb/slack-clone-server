029582ee83e493d4a92207b70267552c4b0a10bd
Sajan Mishra <sjnmishra8@gmail.com>
2018-05-29 20:52:24 +0530

    add db migration and setup tables

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import { makeExecutableSchema } from 'graphql-tools';
 
 import typeDefs from './schema';
 import resolvers from './resolvers';
+import models from './models';
 
 const schema = makeExecutableSchema({
   typeDefs,
@@ -22,4 +23,6 @@ app.use(graphqlEndpoint, bodyParser.json(), graphqlExpress({ schema }));
 
 app.use('/graphiql', graphiqlExpress({ endpointURL: graphqlEndpoint }));
 
-app.listen(PORT);
+models.sequelize.sync().then(() => {
+  app.listen(PORT);
+});
---
+++ models/index.js
@@ -0,0 +1,22 @@
+import Sequelize from 'sequelize';
+
+const sequelize = new Sequelize('slack', 'postgres', 'postgres', {
+  dialect: 'postgres',
+});
+const models = {
+  User: sequelize.import('./user'),
+  Channel: sequelize.import('./channel'),
+  Team: sequelize.import('./team'),
+  Message: sequelize.import('./message'),
+};
+
+Object.keys(models).forEach((modelName) => {
+  if (models[modelName].associate) {
+    models[modelName].associate(models);
+  }
+});
+
+models.sequelize = sequelize;
+models.Sequelize = Sequelize;
+
+export default models;
