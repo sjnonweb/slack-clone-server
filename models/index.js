b30223b8dbd572a048883a12ea8eea2ba0db78af
Sajan Mishra <sjnmishra8@gmail.com>
2018-06-11 18:38:01 +0530

    change postgresql naming convention to underscored

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@ import Sequelize from 'sequelize';
 
 const sequelize = new Sequelize('slack', 'postgres', 'postgres', {
   dialect: 'postgres',
+  underscored: true,
 });
 const models = {
   User: sequelize.import('./user'),
--- models/user.js
+++ models/user.js
@@ -1,20 +1,37 @@
 export default (sequelize, DataTypes) => {
-  const User = sequelize.define('user', {
-    username: {
-      type: DataTypes.STRING,
-      unique: true,
+  const User = sequelize.define(
+    'user',
+    {
+      username: {
+        type: DataTypes.STRING,
+        unique: true,
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+      },
+      password: DataTypes.STRING,
     },
-    email: {
-      type: DataTypes.STRING,
-      unique: true,
+    {
+      underscored: true,
     },
-    password: DataTypes.STRING,
-  });
+  );
 
   User.associate = (models) => {
     User.belongsToMany(models.Team, {
       through: 'member',
-      foreignKey: 'userId',
+      foreignKey: {
+        name: 'userId',
+        field: 'user_id',
+      },
+    });
+    // N:M
+    User.belongsToMany(models.Team, {
+      through: 'channel_member',
+      foreignKey: {
+        name: 'userId',
+        field: 'user_id',
+      },
     });
   };
 
